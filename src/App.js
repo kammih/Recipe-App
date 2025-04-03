c31ccda179191f5e45fa41451770caad9edde84d
Kammi Henderson <kammi_henry@yahoo.com>
2025-04-03 11:34:22 -0500

    Add full recipe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExcerpt";
+import RecipeFull from "./components/Recipefull";
 import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([])
+  const [selectedRecipe, setSelectRecipe] = useState(null)
 
 useEffect(() => {
   const fectchAllRecipes = async () => {
@@ -12,25 +14,46 @@ useEffect(() => {
       const response = await fetch("/api/recipes");
       if (response.ok){
         const data = await response.json();
-        setRecipes(data)
+        setRecipes(data);
       } else {
-        console.log("Oops - could not fetch recipes!")
+        console.log("Oops - could not fetch recipes!", "error");
       }
     } catch(e) {
-      console.error("An error occured during the request", e)
+      console.error("An error occured during the request", e);
+      console.log("An unexpected error occured. Please try again later.", "error");
     }
   };
   fectchAllRecipes();
 },[]);
+
+const handleSelectRecipe = (recipe) => {
+  setSelectRecipe(recipe)
+};
+
+const handleUnselectRecipe = () => {
+  setSelectRecipe(null);
+};
   
   return (
     <div className='recipe-app'>
       <Header />
+      {selectedRecipe && (
+        <RecipeFull 
+        selectedRecipe={selectedRecipe}
+        handleUnselectRecipe={handleUnselectRecipe}
+        />
+      )}
+      {selectedRecipe && (
       <div className='recipe-list'>
       {recipes.map((recipe) => (
-        <RecipeExcerpt key={recipe.id} recipes={recipe} />
+        <RecipeExcerpt
+         key={recipe.id}
+         recipes={recipe}
+         handleSelectRecipe={handleSelectRecipe}
+          />
       ))}
       </div>
+      )}
     </div>
   );
 }
--- src/components/RecipeExcerpt.js
+++ src/components/RecipeExcerpt.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const RecipeExcerpt = ({recipes}) => {
+const RecipeExcerpt = ({recipe, handleSelectRecipe}) => {
     return (
         <article className="recipe-card">
             <figure>
-               <img src={recipes.image_url} alt={recipes.title} />
+               <img src={recipe.image_url} alt={recipe.title} />
             </figure>
-            <h2>{recipes.title}</h2>
-            <p className="flex-spacing">Description: {recipes.description}</p>
-            <button>View</button>
+            <h2>{recipe.title}</h2>
+            <p className="flex-spacing">Description: {recipe.description}</p>
+            <button onClick={() => handleSelectRecipe(recipe)}>View</button>
            </article>
     );
 };
