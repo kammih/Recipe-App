3d4772dcced5d985cd7930bb4eb8f5c33c1260bc
Kammi Henderson <kammi_henry@yahoo.com>
2025-04-08 14:54:22 -0500

    Update search

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
   });
   const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
 useEffect(() => {
   const fectchAllRecipes = async () => {
@@ -157,10 +158,29 @@ const showRecipeForm = () => {
   setShowNewRecipeForm(true);
   setSelectedRecipe(null);
 };
+
+const updateSearchTerm = (text) => {
+  setSearchTerm(text);
+};
+
+const handleSearch = () => {
+  const searchResults = recipes.filter((recipe) => {
+    const valuesToSearch = [recipe.title, recipe.ingredients, recipe.description];
+    return valuesToSearch.some((value) => value.toLowerCase().includes(searchTerm.toLowerCase()));
+  });
+  return searchResults;
+};
+
+const displayedRecipes = searchTerm ? handleSearch() : recipes;
   
   return (
     <div className='recipe-app'>
-      <Header showRecipeForm={showRecipeForm} />
+      <Header 
+      showRecipeForm={showRecipeForm} 
+      updateSearchTerm={updateSearchTerm}
+      searchTerm={searchTerm}
+       />
+
       {showNewRecipeForm && (
        <NewRecipeForm
         newRecipe={newRecipe}
@@ -180,7 +200,7 @@ const showRecipeForm = () => {
       )}
       {!selectedRecipe && (
       <div className='recipe-list'>
-      {recipes.map((recipe) => (
+      {displayedRecipes.map((recipe) => (
         <RecipeExcerpt
          key={recipe.id}
          recipe={recipe}
